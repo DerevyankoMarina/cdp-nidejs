59860b5adf1da0aca86f9d8bd5676b65c406c794
Maryna Derevianko <Maryna_Derevianko@epam.com>
2016-03-28 01:27:21 +0300

    Task03. Not finished

---
+++ 03_nosql_databases_in_nodejs/models/user.js
@@ -0,0 +1,50 @@
+var mongoose = require('../db/mongoose');
+
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: 'First Name is required'
+    },
+    lastName: {
+        type: String,
+        trim: true//,
+        //required: 'Last Name is required'
+    },
+    company: {
+        type: String,
+        trim: true
+    },
+    position: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true//,
+        //required: 'Email address is required',
+        //match: [/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/, 'Please fill a valid email address']
+    },
+    phoneNumber: {
+        type: String,
+        trim: true//,
+        //required: 'Phone Number is required!',
+        //match: [/^[\d\(\)\-\+\s]+$/, 'Please fill a valid phone number']
+    }
+});
+
+schema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+});
+
+schema.set('toJSON', {
+    transform: function(doc, ret, options){
+        ret.id = doc._id;
+        delete ret._id;
+        delete ret.__v;
+    }
+});
+
+module.exports = mongoose.model('User', schema);
---
+++ 03_nosql_databases_in_nodejs/routes/services/users.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../../models/user');
+
+/* GET users listing. */
+/*
+router.get('/', function(req, res, next) {
+    var searchCriterias = {};
+
+    if(req.query.searchBy && req.query.searchValue){
+        searchCriterias[req.query.searchBy] = {
+            '$regex': req.query.searchValue,
+            '$options': 'i'
+        };
+    }
+
+    var query = User.find(searchCriterias)
+        .sort((req.query.sortDir === 'asc' ? '' : '-') + req.query.sortBy);
+
+    query.count(function(err, count){
+        query.skip(req.query.perPage * (req.query.offset - 1))
+        .limit(req.query.perPage)
+        .exec('find', function(err, matchedUsers){
+            if(err){
+                res.status(500).json(err);
+            } else{
+                res.json({
+                    collection: matchedUsers,
+                    total: count
+                });
+            }
+        })
+    })
+});
+*/
+
+/* GET particular user by ID */
+router.get('/api/users/:id', function(req, res, next) {
+    User.findById(req.params.id, function(err, user){
+        if(err){
+            res.status(500).json(err);
+        } else{
+            res.json(user);
+        }
+    })
+});
+
+/* create user */
+router.post('/api/users', function(req, res, next) {
+    console.log(req.body);
+    User.create(req.body, function (err, user) {
+        if(err){
+            res.status(500).json(err);
+        } else{
+            res.json(user);
+        }
+    })
+});
+
+/* update user entry */
+router.put('/api/users/:id', function(req, res, next) {
+    User.findById(req.params.id, function(err, user){
+        if (err) return res.status(500).json(err);
+
+        user.set(req.body);
+
+        user.save(req.body, function(err, user){
+            if (err) return res.status(500).json(err);
+
+            res.json(user);
+        })
+    })
+});
+
+/* delete user entry */
+router.delete('/api/users/:id', function(req, res, next) {
+    User.findByIdAndRemove(req.params.id, function(err, model){
+        if(err){
+            res.status(500).json(err);
+        } else{
+            res.sendStatus(200);
+        }
+    })
+});
+
+/* handle all unsupported methods */
+router.all('/', function(req, res, next) {
+    res.send('Request method is not supported');
+});
+
+module.exports = router;
